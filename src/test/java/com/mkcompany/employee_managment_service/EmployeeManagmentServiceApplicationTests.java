bbc0e45a1f1b21c6fd78eb2816067eb4ddcf150e
mohamed khedr <mk8084721@gmail.com>
2025-09-29 13:19:29 +0300

    Initial commit

---
+++ src/test/java/com/mkcompany/employee_managment_service/EmployeeManagmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkcompany.employee_managment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
